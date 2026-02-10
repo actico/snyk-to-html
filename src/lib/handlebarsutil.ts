9b55ed9d1ee9e0be4718ac31642cb7e7a202cd25
Michael Busch <michael.busch@actico.com>
2026-02-10 18:25:47 +0100

    DEVOP-1060 (bugfix): Correctly handle project names for multiproject scans

--- src/lib/handlebarsutil.ts
+++ src/lib/handlebarsutil.ts
@@ -8,4 +8,7 @@ export function registerHandlebarsHelpers() {
   Handlebars.registerHelper('join', function (array, separator) {
     return Array.isArray(array) ? array.join(separator) : '';
   });
+  Handlebars.registerHelper('isArray', function (value) {
+    return Array.isArray(value);
+  });
 }
